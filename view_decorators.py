07aa357f6877dcef9bfbff7d0f92a7ec51287087
goodude123 <assasinmcpl@gmail.com>
2019-02-20 17:49:17 +0100

    View decorators:   - login_required - user have to login before access website   - deny_logged - If user is logged dont allow him to see website

---
+++ view_decorators.py
@@ -0,0 +1,32 @@
+from flask import redirect, url_for, session
+from functools import wraps
+
+
+def login_required(function):
+    """User must login before access page"""
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if is_logged():
+            return function(*args, **kwargs)
+        return redirect(url_for('.login'))
+    return wrapper
+
+
+def deny_logged(function):
+    """If user is logged dont allow him to see page"""
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if is_logged():
+            return redirect(url_for('.main'))
+        return function(*args, **kwargs)
+    return wrapper
+
+
+def is_logged():
+    """Check is user logged"""
+    try:
+        if session['logged_in']:
+            return True
+    except KeyError as e:
+        pass
+    return False
