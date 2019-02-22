749aef8a6b59e1ac2a933bb29584d2dfa123bf03
goodude123 <assasinmcpl@gmail.com>
2019-02-22 17:33:06 +0100

    Renamed imports, moved class User from models file.

--- views.py
+++ views.py
@@ -2,8 +2,7 @@ import os
 from flask import g, render_template, request, session, url_for, flash, redirect, Blueprint
 from werkzeug.utils import secure_filename
 from view_decorators import login_required, deny_logged
-from models import User
-import image_hosting
+import app
 
 views = Blueprint('views', __name__, template_folder='templates')
 
@@ -11,7 +10,7 @@ views = Blueprint('views', __name__, template_folder='templates')
 @views.route('/', methods=['GET'])
 @deny_logged
 def unlogged():
-    return render_template('login.html')
+    return redirect(url_for('.login'))
 
 
 @views.route('/login', methods=['GET', 'POST'])
@@ -70,9 +69,9 @@ def upload_image():
         if file.filename == '':
             flash('No selected images')
             return redirect(request.url)
-        if file and image_hosting.allowed_file(file.filename):
+        if file and app.allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(image_hosting.app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(app.app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('.display_images'))
 
     return render_template('image_upload.html')
@@ -82,8 +81,34 @@ def upload_image():
 @login_required
 def display_images():
     filenames = []
-    for (dirpath, dirnames, files) in os.walk(image_hosting.get_upload_folder()):
+    for (dirpath, dirnames, files) in os.walk(app.get_upload_folder()):
         for file in files:
             filename = 'received/images/' + file
             filenames.append(filename)
     return render_template('images_display.html', filenames=filenames)
+
+
+class User:
+    class UserAlreadExistsException(Exception): 
+        def __str__(self):
+            return 'User with that username already exists!'
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.table = 'users'
+
+    def save(self):
+        if app.get_object(self.table, **{'username': self.username}) == []:
+            sql = "INSERT INTO users (username, password) VALUES (?, ?);"
+            db = app.get_db()  # while inserting have to call db.commit (connection)
+            cursor = app.get_cursor()
+            cursor.execute(sql, (self.username, self.password))
+            db.commit()  # connection commit
+        else:
+            return self.UserAlreadExistsException()
+
+    def is_created(self):
+        if app.get_object(self.table, **{'username': self.username}):
+            return True
+        return False
